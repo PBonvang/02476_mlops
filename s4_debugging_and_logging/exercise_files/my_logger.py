92d91f91e24f3dfdf1cc1722959c81217d3e1a49
PBonvang <peter.bonvang@gmail.com>
2026-01-08 10:42:46 +0100

    S4 M14: Application logging exercises

--- s4_debugging_and_logging/exercise_files/my_logger.py
+++ s4_debugging_and_logging/exercise_files/my_logger.py
@@ -1,4 +1,11 @@
+import sys
 from loguru import logger
+from logging import WARNING
+
+log_level = WARNING
+logger.remove()  # Remove the default logger
+logger.add(sys.stdout, level=log_level)  # Add a new logger with WARNING level
+logger.add("my_log.log", level=log_level, rotation="100 MB")  # Log all levels to a file
 
 logger.debug("Used for debugging your code.")
 logger.info("Informative messages from your code.")
